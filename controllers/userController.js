76c440c4f633efbff4beca37b3508835aa71ff3c
Desmond Obisi <desmondcutest@yahoo.com>
2023-05-28 03:20:37 +0100

    update: remove uneccesary code blocks

--- controllers/userController.js
+++ controllers/userController.js
@@ -149,7 +149,7 @@ export async function login(req, res) {
 
                         return res.status(200).send({
                             msg: "Login Successful...!",
-                            username: user.username,
+                            user: user,
                             token
                         });
 
@@ -164,102 +164,5 @@ export async function login(req, res) {
 
     } catch (error) {
         return res.status(500).send({ error });
-    }
-}
-
-
-/** GET: http://localhost:8080/api/user/example123 */
-export async function getUser(req, res) {
-
-    const { username } = req.params;
-
-    try {
-
-        if (!username) return res.status(501).send({ error: "Invalid Username" });
-
-        UserModel.findOne({ username }, function (err, user) {
-            if (err) return res.status(500).send({ err });
-            if (!user) return res.status(501).send({ error: "Couldn't Find the User" });
-
-            /** remove password from user */
-            // mongoose return unnecessary data with object so convert it into json
-            const { password, ...rest } = Object.assign({}, user.toJSON());
-
-            return res.status(201).send(rest);
-        })
-
-    } catch (error) {
-        return res.status(404).send({ error: "Cannot Find User Data" });
-    }
-
-}
-
-
-/** PUT: http://localhost:8080/api/user/update 
- * @param: {
-  "header" : "<token>"
-}
-body: {
-    firstName: '',
-    address : '',
-    profile : ''
-}
-*/
-export async function updateUser(req, res) {
-    try {
-
-        // const id = req.query.id;
-        const { userId } = req.body;
-
-        if (userId) {
-            const body = req.body;
-
-            // update the data
-            UserModel.updateOne({ _id: userId }, body, function (err, data) {
-                if (err) throw err;
-
-                return res.status(201).send({ msg: "Record Updated...!" });
-            })
-
-        } else {
-            return res.status(401).send({ error: "User Not Found...!" });
-        }
-
-    } catch (error) {
-        return res.status(401).send({ error });
-    }
-}
-
-/** DELETE: http://localhost:8080/api/user/delete 
- * @param: {
-  "header" : "<token>"
-}
-body: {
-    firstName: '',
-    address : '',
-    profile : ''
-}
-*/
-export async function deleteUser(req, res) {
-    try {
-
-        // const id = req.query.id;
-        const { userId } = req.params;
-
-        if (userId) {
-
-            // update the data
-            UserModel.deleteOne({ _id: userId }, function (err, data) {
-                if (err) throw err;
-
-                return res.status(201).send({ msg: "Record Deleted...!" });
-            })
-
-        } else {
-            return res.status(401).send({ error: "User Not Found...!" });
-        }
-
-    } catch (error) {
-        return res.status(401).send({ error });
     }
 }
--- router/user.js
+++ router/user.js
@@ -13,18 +13,5 @@ userRouter.route('/login').post(controller.login); // login in app
 userRouter.route('/verify').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
 
 
-/** GET Methods */
-
-userRouter.route('/:username').get(controller.getUser) // user with username
-
-/** PUT Methods */
-
-userRouter.route('/update').put(controller.updateUser); // is use to update the user profile
-
-/** DELETE Methods */
-
-userRouter.route('/:userId').delete(controller.deleteUser);
-
-
 
 export default userRouter;
